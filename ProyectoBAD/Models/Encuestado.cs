5a18c30b84f190d3abcf529ed5b43ee97cadfa19
LF18010 <lf18010@ues.edu.sv>
2024-06-07 20:53:28 -0600

    fix Encuestado

--- ProyectoBAD/Models/Encuestado.cs
+++ ProyectoBAD/Models/Encuestado.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoBAD.Models
 {
@@ -11,10 +12,36 @@ namespace ProyectoBAD.Models
         }
 
         public int IdEncuestado { get; set; }
-        public string EmailEncuestado { get; set; } = null!;
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
+        [Display(Name = "Correo Electrónico")]
+        public string EmailEncuestado { get; set; }
+
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
+        [DataType(DataType.Date)]
+        [FechaNoFutura(ErrorMessage = "La fecha de nacimiento no puede ser una fecha futura.")]
+        [Display(Name = "Fecha de Nacimiento")]
         public DateTime FechaNacEncuesta { get; set; }
-        public string? GenEncuestado { get; set; }
+
+        [Required(ErrorMessage = "El género es obligatorio.")]
+        [Display(Name = "Género")]
+        public string GenEncuestado { get; set; }
 
         public virtual ICollection<Respuestum> Respuesta { get; set; }
+        public class FechaNoFuturaAttribute : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                if (value != null)
+                {
+                    DateTime fechaIngresada = (DateTime)value;
+                    if (fechaIngresada > DateTime.Now)
+                    {
+                        return new ValidationResult("La fecha de nacimiento no puede ser una fecha futura.");
+                    }
+                }
+                return ValidationResult.Success;
+            }
+        }
     }
 }
--- ProyectoBAD/obj/Debug/net6.0/ProyectoBAD.AssemblyInfo.cs
+++ ProyectoBAD/obj/Debug/net6.0/ProyectoBAD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoBAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0cc684f8a2291e790c5fff4477c7dd8745d0b20f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoBAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoBAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
