ba4a6ed3d9b5aadfb891d387911918fca6825046
LF18010 <lf18010@ues.edu.sv>
2024-06-07 19:02:34 -0600

    encuestado validado

--- ProyectoBAD/Models/Encuestado.cs
+++ ProyectoBAD/Models/Encuestado.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProyectoBAD.Models
 {
@@ -11,10 +13,36 @@ namespace ProyectoBAD.Models
         }
 
         public int IdEncuestado { get; set; }
+        [Required(ErrorMessage = "El correo electrónico es obligatorio.")]
+        [EmailAddress(ErrorMessage = "El formato del correo electrónico no es válido.")]
+        [Display(Name = "Correo Electrónico")]
         public string EmailEncuestado { get; set; } = null!;
+        [Required(ErrorMessage = "La fecha de nacimiento es obligatoria.")]
+        [DataType(DataType.Date)]
+        [FechaNoFutura(ErrorMessage = "La fecha de nacimiento no puede ser una fecha futura.")]
+        [Display(Name = "Fecha de Nacimiento")]
         public DateTime FechaNacEncuesta { get; set; }
+        [Required(ErrorMessage = "El género es obligatorio.")]
+        [Display(Name = "Género")]
         public string? GenEncuestado { get; set; }
 
         public virtual ICollection<Respuestum> Respuesta { get; set; }
+    }
+}
+
+
+public class FechaNoFuturaAttribute : ValidationAttribute
+{
+    protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+        if (value != null)
+        {
+            DateTime fechaIngresada = (DateTime)value;
+            if (fechaIngresada > DateTime.Now)
+            {
+                return new ValidationResult("La fecha de nacimiento no puede ser una fecha futura.");
+            }
+        }
+        return ValidationResult.Success;
     }
 }
--- ProyectoBAD/obj/Release/net6.0/ProyectoBAD.AssemblyInfo.cs
+++ ProyectoBAD/obj/Release/net6.0/ProyectoBAD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoBAD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3a745c52f92360bbb606fbfd483928fc5179249")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoBAD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoBAD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
